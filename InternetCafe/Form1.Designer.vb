994065eab0ac6ff73ac33e7bd28ab0e9da798e64
William Wells <37521669+WWELLS3@users.noreply.github.com>
2019-03-23 04:47:37 -0500

    Add files via upload

---
+++ InternetCafe/Form1.Designer.vb
@@ -0,0 +1,199 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class MainForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(MainForm))
+        Me.calculateButton = New System.Windows.Forms.Button()
+        Me.clearButton = New System.Windows.Forms.Button()
+        Me.exitButton = New System.Windows.Forms.Button()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.lastNameTextBox = New System.Windows.Forms.TextBox()
+        Me.firstNameTextBox = New System.Windows.Forms.TextBox()
+        Me.hoursTextBox = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.amountDueLabel = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'calculateButton
+        '
+        Me.calculateButton.AccessibleName = ""
+        Me.calculateButton.Location = New System.Drawing.Point(540, 50)
+        Me.calculateButton.Name = "calculateButton"
+        Me.calculateButton.Size = New System.Drawing.Size(75, 32)
+        Me.calculateButton.TabIndex = 0
+        Me.calculateButton.Text = "Calculate"
+        Me.calculateButton.UseVisualStyleBackColor = True
+        '
+        'clearButton
+        '
+        Me.clearButton.Location = New System.Drawing.Point(540, 88)
+        Me.clearButton.Name = "clearButton"
+        Me.clearButton.Size = New System.Drawing.Size(75, 32)
+        Me.clearButton.TabIndex = 1
+        Me.clearButton.Text = "Clear"
+        Me.clearButton.UseVisualStyleBackColor = True
+        '
+        'exitButton
+        '
+        Me.exitButton.Location = New System.Drawing.Point(540, 126)
+        Me.exitButton.Name = "exitButton"
+        Me.exitButton.Size = New System.Drawing.Size(75, 32)
+        Me.exitButton.TabIndex = 2
+        Me.exitButton.Text = "Exit"
+        Me.exitButton.UseVisualStyleBackColor = True
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
+        Me.PictureBox1.Location = New System.Drawing.Point(12, 22)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(174, 174)
+        Me.PictureBox1.TabIndex = 3
+        Me.PictureBox1.TabStop = False
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(284, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(288, 31)
+        Me.Label1.TabIndex = 4
+        Me.Label1.Text = "Cyberian Internet Café"
+        Me.Label1.TextAlign = System.Drawing.ContentAlignment.TopCenter
+        '
+        'lastNameTextBox
+        '
+        Me.lastNameTextBox.Location = New System.Drawing.Point(339, 50)
+        Me.lastNameTextBox.Name = "lastNameTextBox"
+        Me.lastNameTextBox.Size = New System.Drawing.Size(174, 20)
+        Me.lastNameTextBox.TabIndex = 5
+        '
+        'firstNameTextBox
+        '
+        Me.firstNameTextBox.Location = New System.Drawing.Point(339, 76)
+        Me.firstNameTextBox.Name = "firstNameTextBox"
+        Me.firstNameTextBox.Size = New System.Drawing.Size(174, 20)
+        Me.firstNameTextBox.TabIndex = 6
+        '
+        'hoursTextBox
+        '
+        Me.hoursTextBox.Location = New System.Drawing.Point(339, 102)
+        Me.hoursTextBox.Name = "hoursTextBox"
+        Me.hoursTextBox.Size = New System.Drawing.Size(100, 20)
+        Me.hoursTextBox.TabIndex = 7
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(275, 53)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(58, 13)
+        Me.Label2.TabIndex = 9
+        Me.Label2.Text = "Last Name"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(276, 76)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(57, 13)
+        Me.Label3.TabIndex = 10
+        Me.Label3.Text = "First Name"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(246, 105)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(87, 13)
+        Me.Label4.TabIndex = 11
+        Me.Label4.Text = "Number of Hours"
+        '
+        'amountDueLabel
+        '
+        Me.amountDueLabel.BackColor = System.Drawing.Color.White
+        Me.amountDueLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.amountDueLabel.Location = New System.Drawing.Point(339, 138)
+        Me.amountDueLabel.MaximumSize = New System.Drawing.Size(200, 30)
+        Me.amountDueLabel.MinimumSize = New System.Drawing.Size(2, 2)
+        Me.amountDueLabel.Name = "amountDueLabel"
+        Me.amountDueLabel.Size = New System.Drawing.Size(100, 20)
+        Me.amountDueLabel.TabIndex = 12
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(267, 145)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(66, 13)
+        Me.Label5.TabIndex = 13
+        Me.Label5.Text = "Amount Due"
+        '
+        'MainForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(647, 210)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.amountDueLabel)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.hoursTextBox)
+        Me.Controls.Add(Me.firstNameTextBox)
+        Me.Controls.Add(Me.lastNameTextBox)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.exitButton)
+        Me.Controls.Add(Me.clearButton)
+        Me.Controls.Add(Me.calculateButton)
+        Me.Name = "MainForm"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Customer Billing"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents calculateButton As Button
+    Friend WithEvents clearButton As Button
+    Friend WithEvents exitButton As Button
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents lastNameTextBox As TextBox
+    Friend WithEvents firstNameTextBox As TextBox
+    Friend WithEvents hoursTextBox As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents amountDueLabel As Label
+    Friend WithEvents Label5 As Label
+End Class
---
+++ InternetCafe/Form1.vb
@@ -0,0 +1,46 @@
+﻿'Project Name: Internet Cafe
+'Project purpose: performs customer billing for the Internet Cafe'
+'Created by: Will Wells
+
+
+Public Class MainForm
+
+    Const HOURLY_RATE = 12.0
+
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles calculateButton.Click
+
+        Dim hours As Double
+        Dim amount As Double
+        Dim isConverted As Boolean
+
+        isConverted = Double.TryParse(hoursTextBox.Text, hours)
+        If isConverted Then
+            amount = hours * HOURLY_RATE
+
+        End If
+
+        amountDueLabel.Text = Format(amount, "Currency")
+
+        Dim SAPI
+        SAPI = CreateObject("SAPI.spvoice")
+        SAPI.Speak(amountDueLabel)
+
+    End Sub
+
+    Private Sub clearButton_Click(sender As Object, e As EventArgs) Handles clearButton.Click
+        'clears screen for next user
+
+        lastNameTextBox.Text = String.Empty
+        firstNameTextBox.Text = String.Empty
+        hoursTextBox.Text = String.Empty
+        amountDueLabel.Text = String.Empty
+
+    End Sub
+
+    Private Sub exitButton_Click(sender As Object, e As EventArgs) Handles exitButton.Click
+        Me.Close()
+
+    End Sub
+
+End Class
